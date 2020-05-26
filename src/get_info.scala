850b8ca12743716fa1d99f34a5ea1c47c5a16010
Roberto Martínez <rmartinez.igf@gmail.com>
2020-05-26 02:06:41 -0500

    Adding mayoral availability

--- src/get_info.scala
+++ src/get_info.scala
@@ -20,19 +20,23 @@ object ScalaJdbcConnectSelect {
     Class.forName(driver)
     connection = DriverManager.getConnection(url, username, password)
     val statement = connection.createStatement()
-    val query = "SELECT srtext FROM spatial_ref_sys"
+    val q1 = "SELECT  DISTINCT \"limite-de-las-alcaldias\".nomgeo "
+    val q2 = "FROM \"limite-de-las-alcaldias\", \"prueba_fetchdata_metrobus\" "
+    val q3 = "WHERE ST_CONTAINS(\"limite-de-las-alcaldias\".geom, \"prueba_fetchdata_metrobus\".geom) = True "
+    val q4 = "ORDER BY \"limite-de-las-alcaldias\".nomgeo "
+    val query = q1 + q2 + q3 + q4
     val resultSet = statement.executeQuery(query)
     
     while ( resultSet.next() ) {
-        val srt = resultSet.getString("srtext")
-        println("> " + srt )
+        val alcaldias = resultSet.getString("nomgeo")
+        println("> " + alcaldias )
     }
 	} 
 	catch {
     	case e: Exception => e.printStackTrace
 	} finally {
-    // Closing database connection
-    connection.close()
+		// Closing database connection
+		connection.close()
 	}
-  }
+}
 }
