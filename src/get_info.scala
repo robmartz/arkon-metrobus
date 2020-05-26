68370c9776236decfcb4225f2836f470fa3598bd
Roberto Martínez <rmartinez.igf@gmail.com>
2020-05-26 01:22:16 -0500

    Adding scala code for db connection

---
+++ src/get_info.scala
@@ -0,0 +1,36 @@
+package jdbc
+
+import java.sql.DriverManager
+import java.sql.Connection
+
+/**
+ * A Scala JDBC connection
+ */
+object ScalaJdbcConnectSelect {
+	def main(args: Array[String]) {
+    // connect to the database named "gis" on the docker container
+    val driver = "org.postgresql.Driver"
+    val url = "jdbc:postgresql://mbus_db_container:5432/gis"
+    val username = "mbus_user"
+    val password = "mbus123"
+
+    var connection:Connection = null
+    Class.forName(driver)
+    connection = DriverManager.getConnection(url, username, password)
+    val statement = connection.createStatement()
+    val query = "SELECT srtext FROM spatial_ref_sys"
+    val resultSet = statement.executeQuery(query)
+    while ( resultSet.next() ) {
+        val srt = resultSet.getString("srtext")
+        println("> " + srt )
+    }
+    connection.close()
+  }
+}
+
+
+
+
+    
+
+
